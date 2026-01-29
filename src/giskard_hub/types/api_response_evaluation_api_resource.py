2a07cd362b05b1f56dc0828b1ffb5627d81bccc6
Henrique Chaves <henrique@giskard.ai>
2026-01-29 18:28:31 +0100

    fix types

--- src/giskard_hub/types/api_response_evaluation_api_resource.py
+++ src/giskard_hub/types/api_response_evaluation_api_resource.py
@@ -1,18 +1,8 @@
-from typing import Dict, Union, Optional
-
 from .._models import BaseModel
-from ..types.agent import Agent
-from ..types.dataset import Dataset
 from .evaluation_api_resource import EvaluationAPIResource
 
-__all__ = ["APIResponseEvaluationAPIResource", "IncludedItem"]
-
-
-class IncludedItem(BaseModel):
-    data: Union[Agent, Dataset]
+__all__ = ["APIResponseEvaluationAPIResource"]
 
 
 class APIResponseEvaluationAPIResource(BaseModel):
     data: EvaluationAPIResource
-
-    included: Optional[Dict[str, Dict[str, IncludedItem]]] = None
